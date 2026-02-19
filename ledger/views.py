20353fa493e06878a5a39774d6ec5d8d2b70fe53
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-02-19 20:25:54 +0800

    Update recipe_list and recipe_detail views

--- ledger/views.py
+++ ledger/views.py
@@ -1,23 +1,10 @@
-import json
-import os
-from django.shortcuts import render
-from django.conf import settings
-
-# Create your views here.
-
-def load_json(filename):
-    file_path = os.path.join(settings.BASE_DIR, 'ledger', 'data', filename)
-    with open(file_path, 'r') as file:
-        return json.load(file)
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 def recipe_list(request):
-    context = load_json('recipes_list.txt')
-    return render(request, 'ledger/recipe_list.html', context)
+    recipes = Recipe.objects.all()
+    return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
 
-def recipe_1(request):
-    context = load_json('recipe1.txt')
-    return render(request, 'ledger/recipe_detail.html', context)
-
-def recipe_2(request):
-    context = load_json('recipe2.txt')
-    return render(request, 'ledger/recipe_detail.html', context)
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'ledger/recipe_detail.html', {'recipe': recipe})
