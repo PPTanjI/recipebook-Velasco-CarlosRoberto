f0ee4bc5d8dcf7353e548fe6ce6d3e574f7f9ad3
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-02 17:10:38 +0800

    Add minimuml length validator for short bio in models

--- ledger/models.py
+++ ledger/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
@@ -9,7 +10,10 @@ class Profile(models.Model):
         related_name='profile'
     )
     name = models.CharField(max_length=50)
-    short_bio = models.TextField()
+    short_bio = models.TextField(
+        validators=[MinLengthValidator(255)],
+        help_text='Minimum 255 characters'
+    )
 
     def __str__(self):
         return self.name
