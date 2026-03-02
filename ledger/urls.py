e99dcce0141de98cf22bae4ae9dc57e30753d73f
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-02 15:12:37 +0800

    Make final changes and updates

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
-    path('recipes/<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
 
