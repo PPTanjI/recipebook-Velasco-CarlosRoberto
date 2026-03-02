0ce0c0a50469ed9b6c0c18ffaefa5b900d9227df
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-02 15:07:49 +0800

    Protect recipe detail page with login required

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
@@ -5,6 +6,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
 
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'ledger/recipe_detail.html', {'recipe': recipe})
