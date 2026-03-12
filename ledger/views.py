0faed816052961e71bcb3836ead891fb48947695
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-12 18:06:56 +0800

    Add views for recipe creation and adding image

--- ledger/views.py
+++ ledger/views.py
@@ -1,12 +1,45 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe
+from .models import *
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'ledger/recipe_list.html', {'recipes': recipes})
 
+
 @login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'ledger/recipe_detail.html', {'recipe': recipe})
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name']
+    template_name = 'ledger/recipe_add.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    fields = ['image', 'description']
+    template_name = 'ledger/add_image.html'
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe_pk'] = self.kwargs['pk']
+        return context
