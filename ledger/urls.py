8c450817811d75614aa93542ec90a8c12d2685b9
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-12 18:06:19 +0800

    Add recipe creation and image url

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
     path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('add/', views.RecipeCreateView.as_view(), name='recipe_add'),
+    path('<int:pk>/add_image/', views.RecipeImageCreateView.as_view(),
+         name='add_image'),
 ]
-
