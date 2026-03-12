2224b957e2ad0c502a540e4bf3f0d47611b7ff2e
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-12 18:05:17 +0800

    Update for image and fix profile/ingredient

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Profile, Ingredient, RecipeImage
+
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+    extra = 1
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
 
+
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
+
 
 admin.site.register(Recipe, RecipeAdmin)
-
+admin.site.register(Ingredient)
+admin.site.register(Profile)
