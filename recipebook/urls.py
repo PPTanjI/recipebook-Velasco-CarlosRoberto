91407796df1c22a44914f98d2b083a286b0317d9
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-12 18:14:55 +0800

    Make final changes and fixes

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,3 +43,6 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(template_name='ledger/password_reset_complete.html'), 
         name='password_reset_complete'),                                                                            
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
